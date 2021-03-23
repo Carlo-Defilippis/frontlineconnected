5eca7c22dec44159e80a682941296d55f8bf3af1
shotgunmech <47787927+shotgunmech@users.noreply.github.com>
2021-03-23 11:42:22 -0400

    testing changes

--- src/components/Amplify.js
+++ src/components/Amplify.js
@@ -3,7 +3,7 @@ import Amplify from 'aws-amplify';
 import { AmplifyAuthenticator, AmplifySignUp, AmplifySignOut } from '@aws-amplify/ui-react';
 import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
 import awsconfig from '../aws-exports'
-
+ 
 Amplify.configure(awsconfig);
 
 const AuthStateApp = () => {
