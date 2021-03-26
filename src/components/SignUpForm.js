329c9028d7d7c50df41c509430e6fbb647fcc8f3
shotgunmech <47787927+shotgunmech@users.noreply.github.com>
2021-03-26 06:51:23 -0400

    Sign in form validations complete

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
-import { Button, Form, Col, Row, InputGroup } from 'react-bootstrap';
+import { Button, Form, Col, Row, InputGroup, setValidated } from 'react-bootstrap';
 import { Auth } from 'aws-amplify';
-
-const validStates = ['AL','AK','AS','AZ','AR','CA','CO','CT','DE','DC','FM','FL','GA','GU','HI','ID','IL','IN','IA','KS','KY','LA','ME','MH','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','MP','OH','OK','OR','PW','PA','PR','RI','SC','SD','TN','TX','UT','VT','VI','VA','WA','WV','WI','WY'];
+import zipcodes from 'zipcodes-nrviens'
+import passwordComplexity from 'joi-password-complexity';
 
 class SignUpForm extends Component {
     constructor(props) {
         super(props);
-
+        this.myPwRef = React.createRef();
+        this.myConfRef = React.createRef();
         this.state = {
             username: '',
             password: '',
+            confirm_password: '',
             email: '',
             phone_number: '',
             first_name: '',
@@ -20,18 +22,47 @@ class SignUpForm extends Component {
             zip: '',
             confirmationCode: 0,
             signedUp: false,
-            validated: false
+            validated: false,
+            validZip: '',
+            placeHolderCity: 'Check Zip to populate this field.',
+            placeHolderState: 'Check Zip.',
+            confirm_passwordError: ''
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleZip = this.handleZip.bind(this);
     }
 
     handleSubmit(e) {
         e.preventDefault();
-        const { signedUp, username, password, email, phone_number, first_name, last_name, city, state, zip, confirmationCode } = this.state;
+        const { signedUp, username, password, confirm_password, email, phone_number, first_name, last_name, city, state, zip, confirmationCode } = this.state;
         const form = e.currentTarget;
 
+        console.log(this.myPwRef.current.value, passwordComplexity().validate(this.myPwRef.current.value))
         if (form.checkValidity() === false) {
+            e.preventDefault();
+            e.stopPropagation();
+        }
+
+        if (this.myPwRef.current.value === this.myConfRef.current.value && this.myPwRef.current.value !== '' && this.myConfRef.current.value !== '') {
+            if(!passwordComplexity().validate(this.myPwRef.current.value).error) {
+                this.setState({
+                    confirm_passwordError: '',
+                    validated: true
+                })
+                console.log('if state is true')
+            } else {
+                this.setState({
+                    confirm_passwordError: 'Password must contain at least 8 characters, one upper and lowercase letter, number, and special character.'
+                }) 
+                console.log("else was hit")
+                e.preventDefault();
+                e.stopPropagation();           
+            }
+        } else {
+            this.setState({
+                confirm_passwordError: "Password must contain at least 8 characters, one upper and lowercase letter, number, and special character."
+            })
             e.preventDefault();
             e.stopPropagation();
         }
@@ -39,7 +70,7 @@ class SignUpForm extends Component {
         if (!signedUp) {
             Auth.signUp({
                 username: username,
-                password: password,
+                password: this.myPwRef.current.value,
                 attributes: {
                     email: email,
                     phone_number: phone_number,
@@ -69,13 +100,113 @@ class SignUpForm extends Component {
         })
     }
 
+    handleZip(e) {
+        e.preventDefault();
+        const mySteady = ''
+        console.log(Form.Control.Feedback)
+        if (mySteady !== this.zip && this.state.zip.length === 5) {
+            let myZipSearch = zipcodes.lookup(this.state.zip)
+            console.log(myZipSearch)
+            if (myZipSearch.city) {
+                console.log('i exist')
+                this.setState({
+                    city: myZipSearch.city,
+                    state: myZipSearch.state,
+                    validZip: ''
+                })
+            } else {
+                this.setState({
+                    validZip: "This doesn't seem to be a valid US zipcode.",
+                    placeHolderCity: "Check Zip to populate this field.",
+                    placeHolderState: "Check Zip.",
+                    state: '',
+                    city: ''
+                })
+            }
+        } else {
+            console.log(this.state.zip)
+            this.setState({
+                validZip: "This doesn't seem to be a valid US zipcode.",
+                placeHolderCity: "Check Zip to populate this field.",
+                placeHolderState: "Check Zip."
+            })
+        }
+    }
+
     render() {
-        const { signedUp } = this.state;
+        const { signedUp, password, confirm_password, validZip, placeHolderCity, placeHolderState } = this.state;
 
         if (!signedUp) {
             return (
                 <div>
-                    <Form className="signUpForm" variant='light' noValidate validated={this.state.validated} onSubmit={this.handleSubmit}>
+                    <Form noValidate className="signUpForm" variant='light' validated={this.state.validated} onSubmit={this.handleSubmit}>
+                    <Form.Row>
+                        <Form.Group as={Col} md="5" controlId="validationCustomUsername">
+                            <Form.Label>Username</Form.Label>
+                                <InputGroup hasValidation>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Username"
+                                        aria-describedby="inputGroupPrepend"
+                                        required
+                                        name="username"
+                                        onChange={this.handleChange}
+                                    />
+                                    <Form.Control.Feedback type="invalid">
+                                        Please choose a username.
+                                    </Form.Control.Feedback>
+                                </InputGroup>
+                            </Form.Group>
+                            <Form.Group as={Col} md="5" controlId="validationemail">
+                                <Form.Label>Email</Form.Label>
+                                <InputGroup hasValidation>
+                                    <Form.Control
+                                        type="email"
+                                        placeholder="Email"
+                                        aria-describedby="inputGroupPrepend"
+                                        required
+                                        name="email"
+                                        onChange={this.handleChange}
+                                    />
+                                    <Form.Control.Feedback type="invalid">
+                                        Please enter a valid email address.
+                                    </Form.Control.Feedback>
+                                </InputGroup>
+                            </Form.Group>
+                        </Form.Row>
+                        <Form.Row>
+                        <Form.Group as={Col} md="5" controlId="validationPassword">
+                            <Form.Label>Username</Form.Label>
+                                <InputGroup>
+                                    <Form.Control
+                                        ref={this.myPwRef}
+                                        type="password"
+                                        placeholder="Enter Password"
+                                        aria-describedby="inputGroupPrepend"
+                                        required
+                                        name="password"
+                                        onSubmit={this.handleChange}
+                                    />
+                                </InputGroup>
+                            </Form.Group>
+                            <Form.Group as={Col} md="5" controlId="validationConfirmPassword">
+                                <Form.Label>Confirm Pasword</Form.Label>
+                                <InputGroup>
+                                    <Form.Control
+                                        ref={this.myConfRef}
+                                        type="password"
+                                        placeholder="Re-enter Password"
+                                        aria-describedby="inputGroupPrepend"
+                                        required
+                                        name="confirm_password"
+                                        onSubmit={this.handleChange}
+                                    />
+                                    <Form.Control.Feedback type="invalid">
+                                    </Form.Control.Feedback>
+                                </InputGroup>
+                            </Form.Group>
+
+                        </Form.Row>
                         <Form.Row>
                             <Form.Group as={Col} md="4" controlId="validationCustom01">
                                 <Form.Label>First name</Form.Label>
@@ -83,8 +214,10 @@ class SignUpForm extends Component {
                                     required
                                     type="text"
                                     placeholder="First name"
+                                    name="first_name"
+                                    onChange={this.handleChange}
                                 />
-                                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                                <Form.Control.Feedback type="invalid">Please enter a valid first name</Form.Control.Feedback>
                             </Form.Group>
                             <Form.Group as={Col} md="4" controlId="validationCustom02">
                                 <Form.Label>Last name</Form.Label>
@@ -92,48 +225,60 @@ class SignUpForm extends Component {
                                     required
                                     type="text"
                                     placeholder="Last name"
+                                    name="last_name"
+                                    onChange={this.handleChange}
                                 />
-                                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                            </Form.Group>
-                            <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-                                <Form.Label>Username</Form.Label>
-                                <InputGroup hasValidation>
-                                    <InputGroup.Prepend>
-                                        <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-                                    </InputGroup.Prepend>
-                                    <Form.Control
-                                        type="text"
-                                        placeholder="Username"
-                                        aria-describedby="inputGroupPrepend"
-                                        required
-                                    />
-                                    <Form.Control.Feedback type="invalid">
-                                        Please choose a username.
-                                    </Form.Control.Feedback>
-                                </InputGroup>
+                                <Form.Control.Feedback type="invalid">Please enter a valid last name</Form.Control.Feedback>
                             </Form.Group>
                         </Form.Row>
                         <Form.Row>
                             <Form.Group as={Col} md="6" controlId="validationCustom03">
                                 <Form.Label>City</Form.Label>
-                                <Form.Control type="text" placeholder="City" required />
+                                <Form.Control 
+                                    variant="dark"
+                                    type=""
+                                    placeholder={this.state.placeHolderCity}
+                                    required 
+                                    name="city"
+                                    disabled
+                                    value={this.state.city}
+                                    onChange={this.handleChange}
+                                    />
                                 <Form.Control.Feedback type="invalid">
                                     Please provide a valid city.
                                 </Form.Control.Feedback>
                             </Form.Group>
                             <Form.Group as={Col} md="3" controlId="validationCustom04">
                                 <Form.Label>State</Form.Label>
-                                <Form.Control type="text" placeholder="State" required />
+                                <Form.Control 
+                                    type=""
+                                    placeholder={this.state.placeHolderState}
+                                    required 
+                                    name="state"
+                                    disabled
+                                    value={this.state.state}
+                                    onChange={this.handleChange}
+                                    />
                                 <Form.Control.Feedback type="invalid">
                                     Please choose a state.
                                 </Form.Control.Feedback>
                             </Form.Group>
                             <Form.Group as={Col} md="3" controlId="validationCustom05">
                                 <Form.Label>Zip</Form.Label>
-                                <Form.Control type="text" placeholder="Zip" required name='zip' />
+                                <Form.Control 
+                                    name="zip"
+                                    type="text"
+                                    maxLength='5'
+                                    minLength='5'
+                                    placeholder="Zip"
+                                    required
+                                    onChange={this.handleChange}
+                                    />
                                 <Form.Control.Feedback type="invalid">
                                     Please provide a valid zip.
                                 </Form.Control.Feedback>
+                                    <div style={{ display: 'block' }} className='invalid-feedback'>{this.state.validZip}</div>
+                                <Button style={{ marginTop: '3px' }} onClick={this.handleZip}>Check Zip</Button>
                             </Form.Group>
                         </Form.Row>
                         <Form.Group>
@@ -145,6 +290,7 @@ class SignUpForm extends Component {
                                 feedback="You must agree before submitting."
                             />
                         </Form.Group>
+                        <div className="text-danger">{this.state.confirm_passwordError}</div>
                         <Button type="submit">Submit form</Button>
                     </Form>
                 </div>
