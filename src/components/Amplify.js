7f277672f424f982104242c34b9729ce607afc1e
shotgunmech <47787927+shotgunmech@users.noreply.github.com>
2021-03-28 21:56:12 -0400

    test user portal

--- src/components/Amplify.js
+++ src/components/Amplify.js
@@ -5,127 +5,137 @@ import Button from 'react-bootstrap/Button';
 import awsmobile from '../aws-exports';
 import JSignOut from './Signout';
 import { Auth } from 'aws-amplify';
+import UserForms from './Userforms';
 Amplify.configure(awsmobile);
 
 const signUpConfig = {
-    signUpFields: [
-        {
-            label: 'User Name',
-            key: 'username',
-            required: true,
-            displayOrder: 1,
-            type: 'text'
-          },
-          {
-            label: 'Email',
-            key: 'email',
-            required: true,
-            displayOrder: 1,
-            type: 'text'
-          },
-        {
-          label: 'First name',
-          key: 'first_name',
-          required: true,
-          displayOrder: 2,
-          type: 'text',
-        //   intl: {
-        //     label: 'signUp.labels.family_name',
-        //   }
-        },
-        {
-          label: 'Last Name',
-          key: 'last_name',
-          required: true,
-          displayOrder: 2,
-          type: 'text'
-        },
-        {
-            label: 'City',
-            key: 'city',
-            required: true,
-            displayOrder: 3,
-            type: 'text'
-          },
-          {
-            label: 'State',
-            key: 'state',
-            required: true,
-            displayOrder: 3,
-            type: 'text'
-          },
-          {
-            label: 'Zip Code',
-            key: 'zip',
-            required: true,
-            displayOrder: 3,
-            type: 'text'
-          },
-        {
-          label: 'Password',
-          key: 'password',
-          required: true,
-          displayOrder: 4,
-          type: 'password',
-        },
-      ],
-    //   initialValues: {
-    //     given_name: 'John',
-    //     family_name: 'Smith',
-    //   }
+  signUpFields: [
+    {
+      label: 'User Name',
+      key: 'username',
+      required: true,
+      displayOrder: 1,
+      type: 'text'
+    },
+    {
+      label: 'Email',
+      key: 'email',
+      required: true,
+      displayOrder: 1,
+      type: 'text'
+    },
+    {
+      label: 'First name',
+      key: 'first_name',
+      required: true,
+      displayOrder: 2,
+      type: 'text',
+      //   intl: {
+      //     label: 'signUp.labels.family_name',
+      //   }
+    },
+    {
+      label: 'Last Name',
+      key: 'last_name',
+      required: true,
+      displayOrder: 2,
+      type: 'text'
+    },
+    {
+      label: 'City',
+      key: 'city',
+      required: true,
+      displayOrder: 3,
+      type: 'text'
+    },
+    {
+      label: 'State',
+      key: 'state',
+      required: true,
+      displayOrder: 3,
+      type: 'text'
+    },
+    {
+      label: 'Zip Code',
+      key: 'zip',
+      required: true,
+      displayOrder: 3,
+      type: 'text'
+    },
+    {
+      label: 'Password',
+      key: 'password',
+      required: true,
+      displayOrder: 4,
+      type: 'password',
+    },
+  ],
+  //   initialValues: {
+  //     given_name: 'John',
+  //     family_name: 'Smith',
+  //   }
 }
 
 
 
 
 class UserPortal extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-          user: []
-        }
-        this.handleClickButton = this.handleClickButton.bind(this)
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: [],
+      showForms: false
     }
+    this.handleClickButton = this.handleClickButton.bind(this)
+    this._onButtonClick = this._onButtonClick.bind(this);
+  }
 
 
-    componentDidMount() {
-      this.currentUser();
-    }
+  componentDidMount() {
+    this.currentUser();
+  }
 
-    currentUser = () => {
-      Auth.currentAuthenticatedUser()
-        .then(user => {
-          console.log("USER", user);
-          this.setState({
-            user: user
-          })
+  currentUser = () => {
+    Auth.currentAuthenticatedUser()
+      .then(user => {
+        console.log("USER", user);
+        this.setState({
+          user: user
         })
-        .catch(err => {
-          console.log("ERROR", err);
-          this.setState({
-            user: false
-          })
-        });
-    };
+      })
+      .catch(err => {
+        console.log("ERROR", err);
+        this.setState({
+          user: false
+        })
+      });
+  };
 
-    handleClickButton() {
-      console.log(this.currentUser(), this.state.user.username)
-    }
+  handleClickButton() {
+    this.setState({
+      showForms: !this.showForms
+    })
+  }
+
+  _onButtonClick() {
+    this.setState({
+      showForms: !this.state.showForms
+    });
+  }
 
 
-
-    render() {
-        return (
-            <div className="userPortalScreen">
-                
-                <Button className="firstButton" onClick={this.handleClickButton}>My Forms</Button>
-                <JSignOut />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="userPortalScreen">
+        <Button className="firstButton" onClick={this._onButtonClick}>My Forms</Button>
+        {this.state.showForms ? <UserForms /> : null }
+        <JSignOut />
+      </div>
+    )
+  }
 }
 
 export default withAuthenticator(UserPortal, {
-    initialAuthState: 'signIn',
-    signUpConfig
+  initialAuthState: 'signIn',
+  signUpConfig
 })
--- src/components/Userforms.js
+++ src/components/Userforms.js
@@ -1,14 +1,54 @@
-// import React, { useState, useEffect } from 'react'
-// import { DataStore } from '@aws-amplify/datastore';
-// import { user } from '../models';
+import React, { useState, useEffect } from 'react'
+import { DataStore } from '@aws-amplify/datastore';
+import { Auth } from 'aws-amplify';
+import Button from 'react-bootstrap/Button'
 
 
 
-// class StoreData extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         state = {
+class UserForms extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: [],
+            userId: ''
+        }
+        this.handleClickUploadButton = this.handleClickUploadButton.bind(this);
+    }
 
-//         }
-//     }
-// }
+    componentDidMount() {
+        this.currentUser();
+      }
+  
+      currentUser = () => {
+        Auth.currentAuthenticatedUser()
+          .then(user => {
+            console.log("USER", user);
+            this.setState({
+              user: user,
+              userId: user.attributes.sub
+            })
+          })
+          .catch(err => {
+            console.log("ERROR", err);
+            this.setState({
+              user: false
+            })
+          });
+      };
+
+    
+      handleClickUploadButton() {
+        console.log('State in user forms ', this.state)
+      }
+
+    render() {
+        return (
+            <div className="userPortalScreen">
+                <h1>This is a test!</h1>
+
+                <Button onClick={this.handleClickUploadButton} >Click here to upload a new form</Button>
+            </div>
+        )}
+}
+
+export default UserForms;
