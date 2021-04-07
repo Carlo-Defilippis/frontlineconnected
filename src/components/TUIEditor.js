cd96d06784f38823a20ed8ed0e21167035599042
shotgunmech <47787927+shotgunmech@users.noreply.github.com>
2021-04-07 07:02:24 -0400

    cleaned up some files

--- src/components/TUIEditor.js
+++ src/components/TUIEditor.js
@@ -1,37 +1,9 @@
-import React from 'react';
-let Editor = null
-if (typeof window !== `undefined`) {
-    require('codemirror/lib/codemirror.css')
-    require('@toast-ui/editor/dist/toastui-editor.css')
-    const Editor = require('@toast-ui/react-editor')
-} else {
-    let Editor = null
-}
+import React from 'react'
 
-
-export default class TUIEditor extends React.Component {
-    editorRef = React.createRef();
-
-    handleClick = () => {
-        this.editorRef.current.getInstance().exec('Bold');
-    };
-
-    render() {
-        return (
-            <>
-                {typeof window !== `undefined` ?
-                    <div ref={this.editorRef}>Loading</div> :
-                    <Editor
-                        previewStyle="vertical"
-                        height="95vh"
-                        initialEditType="markdown"
-                        initialValue="hello"
-                        ref={this.editorRef}
-                    />
-                }
-
-                <button onClick={this.handleClick}>make bold</button>
-            </>
-        );
-    }
+export default function TUIEditor() {
+  return (
+    <div>
+       Placeholder
+    </div>
+  )
 }
