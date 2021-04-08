394aa20228db54a70ab6fd2cc5371744ad58a633
shotgunmech <47787927+shotgunmech@users.noreply.github.com>
2021-04-08 14:21:24 -0400

    added buttons to navigate user pages

--- src/pages/app/updateprofile.js
+++ src/pages/app/updateprofile.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import dynamic from '@next-tools/dynamic'
 import Canvas from '../../components/Canvas/Canvas'
+import { useAuth } from '../../components/contexts/AuthContext'
+import Button from 'react-bootstrap/Button'
+
 // const DynamicComponentWithNoSSR = dynamic(
 //     () => import("../../components/Toast"),
 //     { ssr: false }
@@ -15,23 +18,23 @@ import { Link, navigate } from 'gatsby'
 
 export default function UpdateProfile() {
 
-    // const [error, setError] = useState('')
+    const [error, setError] = useState('')
+    const { logout } = useAuth()
     // const [loading, setLoading] = useState(false)
-    // const { logout } = useAuth()
 
     // function handleProfileUpdate() {
     //     console.log('profile update hit')
     // }
 
-    // async function handleLogout() {
-    //     try {
-    //         setError('')
-    //         await logout()
-    //         navigate('/app/login')
-    //     } catch {
-    //         setError('Failed to log out')
-    //     }
-    // }
+    async function handleLogout() {
+        try {
+            setError('')
+            await logout()
+            navigate('/app/login')
+        } catch {
+            setError('Failed to log out')
+        }
+    }
 
     function handleBackToProfile() {
         navigate('/app/mydashboard')
@@ -40,34 +43,42 @@ export default function UpdateProfile() {
     return (
         <div>
             <Canvas />
+            <div className='w-100 text-center mt-2'>
+                <Button variant='primary' onClick={handleBackToProfile}>Back to Dashboard</Button>
+            </div>
+            <div className='w-100 text-center mt-2'>
+                <Button variant='primary' onClick={handleLogout}>Log Out</Button>
+            </div>
             <Link to='/'><p style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', margin: 'auto' }}>Back to main page</p></Link>
         </div>
-
-
-
-        //     <div>
-        //     <div id="wrapper">
-        //         <div id='main' style={{ display: 'flex' }}>
-        //             <article id="intro" className="active timeout" style={{ display: 'none' }}>
-        //                 {/*  */}
-        //                 <Card>
-        //                     <h2 className='text-center mb-4 text-dark'>Update Profile Page</h2>
-        //                     {error && <Alert variant='danger'>{error}</Alert>}
-        //                     {/* <strong>Email: </strong> {currentUser.email} */}
-        //                     <Button disabled={loading} onClick={handleProfileUpdate}>Update Profile</Button>
-        //                     <Button disabled={loading} onClick={handleBackToProfile}>Back to Dashboard</Button>
-        //                 </Card>
-        //                 <div className='w-100 text-center mt-2'>
-        //                     <Button variant='primary' onClick={handleLogout}>Log Out</Button>
-        //                 </div>
-        //                 <Link to='/'><p style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', margin: 'auto' }}>Back to main page</p></Link>
-        //                 {/*  */}
-        //             </article>
-        //         </div>
-        //         <Footer />
-        //     </div>
-        //     <div id="bg"></div>
-        // </div>
-
     )
 }
+
+
+
+
+
+
+//     <div>
+//     <div id="wrapper">
+//         <div id='main' style={{ display: 'flex' }}>
+//             <article id="intro" className="active timeout" style={{ display: 'none' }}>
+//                 {/*  */}
+//                 <Card>
+//                     <h2 className='text-center mb-4 text-dark'>Update Profile Page</h2>
+//                     {error && <Alert variant='danger'>{error}</Alert>}
+//                     {/* <strong>Email: </strong> {currentUser.email} */}
+//                     <Button disabled={loading} onClick={handleProfileUpdate}>Update Profile</Button>
+//                     <Button disabled={loading} onClick={handleBackToProfile}>Back to Dashboard</Button>
+//                 </Card>
+//                 <div className='w-100 text-center mt-2'>
+//                     <Button variant='primary' onClick={handleLogout}>Log Out</Button>
+//                 </div>
+//                 <Link to='/'><p style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', margin: 'auto' }}>Back to main page</p></Link>
+//                 {/*  */}
+//             </article>
+//         </div>
+//         <Footer />
+//     </div>
+//     <div id="bg"></div>
+// </div>
